b6634c4ef0f5c8f01ee5dad89d0deeedede7e0b2
Gyanendra Thakur <gyanendrathakur567@gmail.com>
2026-08-02 23:04:09 +0530

    Day 1

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function Header (){
+
+    return (
+        <Header>
+            <div>
+                <h1>This is my web</h1>
+            </div>
+        </Header>
+    )
+
+}
+export default Header
